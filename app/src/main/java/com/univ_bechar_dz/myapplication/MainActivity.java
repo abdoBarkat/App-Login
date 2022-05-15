0b0ba2f9a0f666f87b5cc7f95275bc545a52a3d3
Abdo <barkat.abderrahmane@univ-bechar.dz>
2022-05-15 11:14:36 +0100

    init

---
+++ app/src/main/java/com/univ_bechar_dz/myapplication/MainActivity.java
@@ -0,0 +1,14 @@
+package com.univ_bechar_dz.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+//test
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
